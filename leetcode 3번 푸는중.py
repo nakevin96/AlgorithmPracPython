2cac6e2144e92869db070c6ad5126c8fd01b9956
Carrotww <rjgjfl90@naver.com>
2022-06-10 18:21:15 +0900

    Create leetcode 3번 푸는중.py

---
+++ leetcode 3번 푸는중.py
@@ -0,0 +1,10 @@
+class solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        count = 0
+
+        return count
+
+a = solution()
+print(a.lengthOfLongestSubstring('absabsabs'))
+print(a.lengthOfLongestSubstring('bbbbbb'))
+print(a.lengthOfLongestSubstring('pwwkew'))
