6a0016847628030c3126beb0e384c4adb35902a0
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-06-11 11:07:35 +0900

    Update leetcode 3번 푸는중.py

--- leetcode 3번 푸는중.py
+++ leetcode 3번 푸는중.py
@@ -1,8 +1,18 @@
 class solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        count = 0
+        countlist = []
+        checklist = [s[0]]
+        for i in range(1, len(s)):
+            if s[i] != s[i - 1] and s[i] not in checklist:
+                checklist.append(s[i])
+                countlist.append(len(checklist))
+            elif s[i] == s[i - 1]:
+                countlist.append(len(checklist))
+                checklist.clear()
+                checklist.append(s[i])
 
-        return count
+        return max(countlist)
+
 
 a = solution()
 print(a.lengthOfLongestSubstring('absabsabs'))
