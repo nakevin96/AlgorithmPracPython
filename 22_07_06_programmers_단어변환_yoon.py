099005094fe28038e3a8260a798a4121bb205a6b
2015005169 <77617267+nakevin96@users.noreply.github.com>
2022-07-06 23:29:24 +0900

    단어변환 추가

--- 22_07_06_programmers_단어변환_yoon.py
+++ 22_07_06_programmers_단어변환_yoon.py
@@ -5,19 +5,22 @@ def solution(begin, target, words):
     if target not in words:
         return 0
     queue = deque()
-    queue.append((begin, 0))
+    queue.append((len(words), 0))
+    words.append(begin)
     checked = [0 for _ in range(len(words))]
 
     while queue:
-        next_word, move_val = queue.popleft()
-        if next_word == target:
+        next_word_idx, move_val = queue.popleft()
+        checked[next_word_idx] = 1
+        if words[next_word_idx] == target:
             return move_val
 
         for word_idx in range(len(words)):
             diff_val = 0
             for idx in range(len(target)):
-                if next_word[idx] != words[word_idx][idx]:
+                if words[next_word_idx][idx] != words[word_idx][idx]:
                     diff_val += 1
             if diff_val == 1 and checked[word_idx] == 0:
-                queue.append((words[word_idx], move_val + 1))
+                queue.append((word_idx, move_val + 1))
 
+    return 0
