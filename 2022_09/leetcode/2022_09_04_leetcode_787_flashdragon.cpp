54f090411e93b3224e5c553a7ffce4593ac0fa7a
김용재 <yooh3574@naver.com>
2022-09-04 16:41:24 +0900

    hi

--- 2022_09/leetcode/2022_09_04_leetcode_787_flashdragon.cpp
+++ 2022_09/leetcode/2022_09_04_leetcode_787_flashdragon.cpp
@@ -1,63 +1,62 @@
-#include<iostream>
-#include<cstring>
-#include<string>
-#include<vector>
-#include<algorithm>
-#include<cmath>
-#include<set>
-#include<queue>
-#include<map>
-using namespace std;
-typedef long long ll;
-set<string>s;
-void go(string a, int n);
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	ll t, test_case = 0;
-	cin >> t;
-	for (test_case = 0; test_case < t; test_case++)
-	{
-		string Answer = "";
-		int n, m;
-		cin >> n >> m;
-		string a, b;
-		cin >> a >> b;
-		if (n < m)
-			Answer = "NO";
-		else if (n == m && a == b)
-			Answer = "YES";
-		else if (n == m && a != b)
-			Answer = "NO";
-		else
+
+struct go {
+	int b, cost = -1;
+	int time = -1;
+	bool operator<(const go& temp)const {
+		return cost > temp.cost;
+	}
+};
+vector<go>vt[101];
+int check[101] = { 0 };
+int vist[101][101];
+priority_queue<go>q;
+class Solution {
+public:
+
+	int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+		while (!q.empty())
+			q.pop();
+		memset(vist, -1, sizeof(vist));
+		memset(check, 0, sizeof(check));
+		for (int i = 0; i < n; i++)
 		{
-			
+			vt[i].clear();
 		}
-		cout << "Case #" << test_case + 1 << endl;
-		cout << Answer << endl;
+		for (auto t : flights)
+		{
+			vt[t[0]].push_back({ t[1],t[2] });
+		}
+		q.push({ src,0,0 });
+		while (!q.empty())
+		{
+			int st = q.top().b;
+			int cost = q.top().cost;
+			int time = q.top().time;
+			q.pop();
+			if (time > k + 1)
+				continue;
+			if (st == dst)
+			{
+				return cost;
+			}
+			for (int i = 0; i < vt[st].size(); i++)
+			{
+				int d = vt[st][i].b;
+				int ncost = vt[st][i].cost + cost;
+				int ntime = time + 1;
+				if (vist[d][ntime] == -1 || vist[d][ntime] > ncost)
+				{
+					vist[d][ntime] = ncost;
+					q.push({ d,ncost,ntime });
+				}
+			}
+		}
+		int res = -1;
+		for (int i = 1; i <= k + 1; i++)
+		{
+			if (vist[dst][i] != -1 && (res == -1 || vist[dst][i] < res))
+				res = vist[dst][i];
+		}
+		return res;
 	}
-}
-
-void go(string a, int n)
-{
-	if (n == 0)
-	{
-		s.insert(a);
-		return;
-	}
-	string temp = a;
-	int idx = temp.find("a");
-	if (idx != string::npos)
-	{
-		go(temp.erase(idx), n - 1);
-	}
-	string temp = a;
-	int idx = temp.find("b");
-	if (idx != string::npos)
-	{
-		go(temp.erase(idx), n - 1);
-	}
-
-}
+};
