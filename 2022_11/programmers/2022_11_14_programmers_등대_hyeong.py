8e28b32a41d0da269f7482b433bdb0f379867be5
Carrotww <rjgjfl90@naver.com>
2022-11-15 00:07:23 +0900

    수정

--- 2022_11/programmers/2022_11_14_programmers_등대_hyeong.py
+++ 2022_11/programmers/2022_11_14_programmers_등대_hyeong.py
@@ -7,13 +7,29 @@ def solution(n, lighthouse):
     for a, b in lighthouse:
         path_dict[a].append(b)
         path_dict[b].append(a)
-    
+
+    result = 0
+    while path_dict:
+        for end_node, side_node in path_dict.copy().items():
+            if len(side_node) == 1 and end_node in path_dict:
+                temp = side_node[0]
+                del_node_list = [x for x in path_dict[temp]]
+                del path_dict[temp]
+                for i in del_node_list:
+                    path_dict[i].remove(temp)
+                    if not path_dict[i]:
+                        del path_dict[i]
+                result += 1
+
+    return result
+
+    # 풀이 방식
     # path_dict를 모두 돌면서 key의 value가 하나 있는 곳을 먼저 체크 한다.
     # 체크 한 후 path_dict에 존재하는지도 체크한다 - 없으면 물려있는 상대 노드에 관련있는
     # 모든 노드를 제거 한다.
-    # result += 1을 한다
+    # result += 1을 한식
     
-    result = 0
+    # 풀이의 잔재...
     # while path_dict:
     #     for end_node, side_node in path_dict.items():
     #         # if not side_node:
@@ -48,22 +64,6 @@ def solution(n, lighthouse):
     #     for dl in del_list:
     #         del(path_dict[dl])
     #     need_remove_node = 0
-
-    print(path_dict)
-
-    while path_dict:
-        for end_node, side_node in path_dict.copy().items():
-            if len(side_node) == 1 and end_node in path_dict:
-                temp = side_node[0]
-                del_node_list = [x for x in path_dict[temp]]
-                del path_dict[temp]
-                for i in del_node_list:
-                    path_dict[i].remove(temp)
-                    if not path_dict[i]:
-                        del path_dict[i]
-                result += 1
-
-    return result
 
 print(solution(8, [[1, 2], [1, 3], [1, 4], [1, 5], [5, 6], [5, 7], [5, 8]]))
 print(solution(10, [[4, 1], [5, 1], [5, 6], [7, 6], [1, 2], [1, 3], [6, 8], [2, 9], [9, 10]]))
